d24b5ff8e9c367ff48ec2448f50a203e1fc53eb3
Suvam Naskar <suvamnaskar780@gmail.com>
2023-11-01 14:46:50 +0530

    Create TodoauthApplication.java

---
+++ todoauth/src/main/java/in/suvam/todoauth/TodoauthApplication.java
@@ -0,0 +1,13 @@
+package in.suvam.todoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoauthApplication.class, args);
+	}
+
+}
