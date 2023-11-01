aded1f7537a1f12d65530454dedb36d9b7596f8a
Suvam Naskar <suvamnaskar780@gmail.com>
2023-11-01 14:46:53 +0530

    Create TodoauthApplicationTests.java

---
+++ todoauth/src/test/java/in/suvam/todoauth/TodoauthApplicationTests.java
@@ -0,0 +1,13 @@
+package in.suvam.todoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
