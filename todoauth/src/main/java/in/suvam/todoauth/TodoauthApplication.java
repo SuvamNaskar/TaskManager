7827eb797807fa01e38419eb18b1aafe7f49ee4f
Suvam Naskar <suvamnaskar780@gmail.com>
2023-11-01 14:59:40 +0530

    Test Connection
    Connection successful

--- todoauth/src/main/java/in/suvam/todoauth/TodoauthApplication.java
+++ todoauth/src/main/java/in/suvam/todoauth/TodoauthApplication.java
@@ -1,13 +1,30 @@
 package in.suvam.todoauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class TodoauthApplication {
+public class TodoauthApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate mysql;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		String query = "INSERT INTO tasks (task_name, task_time) VALUES (?, ?)";
+		int rslt = mysql.update(query, "Tailwind", "17:00");
+		if(rslt > 0)
+		{
+			System.out.println("1 row inserted");
+		}
 	}
 
 }
